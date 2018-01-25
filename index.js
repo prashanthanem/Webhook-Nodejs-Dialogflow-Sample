22b76a410d1fab0d0c24a2ebfe2c463222b68782
Prashanth Anem <prashanthanem@gmail.com>
2018-01-25 16:37:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,10 +31,29 @@ ExpServer.post("/orders", function(req, res) {
      });
 	 }
 	});	
-	}else{
+	}
+	else if((req.body.result.action == "updatestatus") && (req.body.result.parameters.ID != "" && req.body.result.parameters.orderstatus != "")){
+	updateorderstatus(req.body.result.parameters.ID, req.body.result.parameters.orderstatus, function(resp){	
+	if(resp){
 	return res.json({
-    speech: "Please provide valid order number",
-    displayText: "Please provide valid order number",
+    speech: resp,
+    displayText: resp,
+    source: "webhook-echo-sample"
+     });
+	 }
+	 else{
+	return res.json({
+    speech: "Please provide valid order number with valid status",
+    displayText: "Please provide valid order number with valid status",
+    source: "webhook-echo-sample"
+     });
+	 }
+	});
+	}
+	else{
+	return res.json({
+    speech: "Under construction",
+    displayText: "Under construction",
     source: "webhook-echo-sample"
      });
 	}
@@ -44,6 +63,23 @@ ExpServer.post("/orders", function(req, res) {
 function gettheorderstatus(id, callback) {
     var options = {
         uri : 'http://35.202.179.196/EventEngine/rest/googlepoc/minifetchorders?orderId='+id,
+        method : 'GET'
+    }; 
+    var respo = '';
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+		respo = body;			
+        }
+        else {            
+		respo = body;	
+        }
+        callback(respo);
+    });
+}
+
+function updateorderstatus(orderid, orderstatus, callback) {
+    var options = {
+        uri : 'http://35.202.179.196/EventEngine/rest/googlepoc/updateOrder?orderId='+orderid+'&&orderStatus='+orderstatus;	
         method : 'GET'
     }; 
     var respo = '';
